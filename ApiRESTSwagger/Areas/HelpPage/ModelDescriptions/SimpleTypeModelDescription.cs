4742b31e70b7cd2c72a4ff6b1df4bdf4a9ce90f8
AdrianDtt90 <adridotta@gmail.com>
2019-04-24 22:08:30 +1000

    API with swagger (without DB)

---
+++ ApiRESTSwagger/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ApiRESTSwagger.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ ApiRESTSwagger/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ApiRESTSwagger.WebApiApplication" Language="C#" %>
